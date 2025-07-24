128a3e3ed534217d2fb812b324b6844d50f84d48
mahlet-16 <mahletamenu16@gmail.com>
2025-07-24 05:07:28 +0300

    Delete BookReview directory

--- BookReview/review/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, BookListCreateAPIView, SubmitReviewView, BookReviewsView
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('books/', BookListCreateAPIView.as_view(), name='book_list'),
-    path('books/<int:book_id>/reviews', BookReviewsView.as_view(), name='book_review'),
-    path('submit-review/', SubmitReviewView.as_view(), name='submit_review'),
-]
